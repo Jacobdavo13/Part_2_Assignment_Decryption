6aab8e384e56b6ba7e05931a4ba739576c6ce904
Jacob Davison <c3324607@uon.edu.au>
2019-04-09 02:43:25 +0000

    works without correction to just upper case or no symbols

--- Decryption.c
+++ Decryption.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
-int main()
-{
-    char b[] = "kbdpc";
+int main(){
+    char b[] = "i`bna";
     int k = 1;
     int index;
+    for(index =  0; b[index] !=0; index++){
+        b[index] = b[index] + k;
+        
+    } 
+    printf("decrypted message is: %s\n", b);
 }
