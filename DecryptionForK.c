9d25a54d236b9d946f0e2879d3efa47e0091d4a3
Jacob Davison <c3324607@uon.edu.au>
2019-04-23 12:18:05 +0000

    Proper comments used

--- DecryptionForK.c
+++ DecryptionForK.c
@@ -2,11 +2,19 @@
 void DecK(char message[], int key); //Function prototype for DecK (Decryotion for K)
 int main() 
 {
-    char message[] = "SSTTUU"; //inputted message to be decrypted
+    char message[] = "SSTTUooooo"; //inputted message to be decrypted
     int key = 100; //rotational cipher key
     DecK(message, key); //Function which prints decrypted message from key and message provided
 }
- 
+
+/*The function DecK perfroms a rotation cipher decryption on an inputted char string of characters
+The char input is a string of inputted characters that has been encrypted using a rotation cipher
+The int input is the key and is the amount of spaces that the encrypted letters have rotated from their original values (Eg. if A goes to B then key = 1)
+It returns a void value as this function prints the decrypted message string.
+This function takes an encrypted message and the rotation key used for encryption and decrypts the message by moving the letter back to their original places
+This means that if the enecryption key used was 1 then the letter B decrypted would be A
+This function does not have any data type restrictions but string length is limited to how much the computer/website can take without crashing
+ */
 void DecK(char message[], int key) 
 {
     printf("Inputted message was: %s\n", message);
@@ -32,7 +40,8 @@ void DecK(char message[], int key)
         else //if the characters are upper case letters this occurs
         {
             message[index] = ((message[index] - 65) - key + 26) % 26 + 65; //turning the ASCII character to a number between 0 and 25 through -65 and then taking away the key to move the letter back to where it was before it was encrypted
-//
+//+26 was done to ensure that when key is taken away from the value it still produces a value that is postive and can be used by the function
+//modulus 26 makes produces the remainder of the created value divided by 26 and 65 is added to produce a the decrypted ASCII value
         }
          
     }
